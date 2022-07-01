dcee6f2f3282bbfb4d020bc3d1d18a8d18d1c991
Santi <santiloureiro12@gmail.com>
2022-06-30 23:25:35 -0300

    Este es el desafio de Bucles For

--- desafio.js
+++ desafio.js
@@ -2,6 +2,8 @@ let numeros = [0, 2, 4, 6, 7, 9, 10, 15, 26 , 32 ,45];
 let numerosPares = [];
 let numerosImpares = [];
 
+alert("Queres ver cuales de estos numeros son pares y cuales impares?" + numeros +"\nCliquea aceptar! \n")
+
 for (let i = 0; i <= 10 ; i++) {
     if((numeros[i] % 2) === 0){
         numerosPares.push(numeros[i]);
@@ -11,4 +13,6 @@ for (let i = 0; i <= 10 ; i++) {
         }
 }
 
-console.log(numerosPares, numerosImpares);
+alert("Estos son los numeros pares \n" + numerosPares + "\nEstos son los numeros impares \n" + numerosImpares);
+
+
